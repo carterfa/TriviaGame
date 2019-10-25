31d4609f19819b905968d1f527ef245d68bea936
Farrah Carter <carterfarraha@gmail.com>
2019-10-24 22:02:50 -0400

    Added countdown.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//array of quiz questions
 let quizArray = [
     {
         title: "What is a banana?",
@@ -27,9 +28,26 @@ let i = 0;
 let currentQ = quizArray[i];
 let correctNum = 0;
 let wrongNum = 0;
+let timer = 20;
+let intervalId;
 
 //displays question with options
 function displayQuestion(objQuiz) {
+
+    //reset the clock
+    timer = 20;
+
+    //start the clock
+    intervalId = setInterval(function () {
+        $("#countdown").text(timer);
+        timer--;
+
+        if (timer == 0) {
+            $("#countdown").text("TIMES UP!");
+            wrongDisplay();
+        }
+
+    }, 1000);
 
     //clears and adds question title
     $("#questionTitle").text("");
@@ -46,22 +64,46 @@ function displayQuestion(objQuiz) {
 
 }
 
+//displays correct answer dialog
 function correctDisplay() {
+    clearInterval(intervalId);
+
     $("#questionTitle").text("Correct");
     i++;
     correctNum++;
     currentQ = quizArray[i];
-    setTimeout(function(){ displayQuestion (currentQ); }, 3000);
+
+    if (i == quizArray.length) {
+        setTimeout(function () { displayOver(); }, 3000);
+    } else { setTimeout(function () { displayQuestion(currentQ); }, 3000); }
 }
 
+//displays wrong answer dialog
 function wrongDisplay() {
+    clearInterval(intervalId);
     //grabs correct answer
-    let correctA = $("#"+currentQ.correct).text();
-    $("#questionTitle").text("Wrong! The correct answer was: " +correctA);
+    let correctA = $("#" + currentQ.correct).text();
+    $("#questionTitle").text("The correct answer was: " + correctA);
     i++;
     wrongNum++;
     currentQ = quizArray[i];
-    setTimeout(function(){ displayQuestion (currentQ); }, 3000);
+
+    if (i == quizArray.length) {
+        setTimeout(function () { displayOver(); }, 3000);
+    } else { setTimeout(function () { displayQuestion(currentQ); }, 3000); }
+}
+
+//displays final screen
+function displayOver() {
+    $("#optionsBox").empty();
+
+    $("#questionTitle").text("All Done!");
+
+    $("#messageBox").append("<h2>Correct Answers: " + correctNum + "</h2>");
+    $("#messageBox").append("<h2>Wrong Answers: " + wrongNum + "</h2>");
+
+    $("#countdown").text("");
+
 }
 
 $(document).ready(function () {
@@ -77,16 +119,6 @@ $(document).ready(function () {
             wrongDisplay();
         }
 
-
-
-
-
     })
-
-
-
-
-
-
 
 });
