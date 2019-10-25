8a0e44d56bfcf9adc57d602aff2b8ad63e1bf976
Farrah Carter <carterfarraha@gmail.com>
2019-10-24 20:56:30 -0400

    Added time out that moves on to next question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,50 +8,84 @@ let quizArray = [
             d: "A fruit."
         },
         correct: "d"
+    },
+
+    {
+        title: "What is a man?",
+        options: {
+            a: "An airplane.",
+            b: "A friend.",
+            c: "Tasty.",
+            d: "A potato."
+        },
+        correct: "b"
     }
 
 ]
 
+let i = 0;
+let currentQ = quizArray[i];
+let correctNum = 0;
+let wrongNum = 0;
+
 //displays question with options
 function displayQuestion(objQuiz) {
 
+    //clears and adds question title
+    $("#questionTitle").text("");
     $("#questionTitle").text(objQuiz.title);
 
+    //clears options
     $("#optionsBox").empty();
 
-    $("#optionsBox").append("<h2 class='choice' id='a'>"+objQuiz.options.a+"</h2>");
-    $("#optionsBox").append("<h2 class='choice' id='b'>"+objQuiz.options.b+"</h2>");
-    $("#optionsBox").append("<h2 class='choice' id='c'>"+objQuiz.options.c+"</h2");
-    $("#optionsBox").append("<h2 class='choice' id='d'>"+objQuiz.options.d+"</h2>");
+    //adds question options
+    $("#optionsBox").append("<h2 class='choice' id='a'>" + objQuiz.options.a + "</h2>");
+    $("#optionsBox").append("<h2 class='choice' id='b'>" + objQuiz.options.b + "</h2>");
+    $("#optionsBox").append("<h2 class='choice' id='c'>" + objQuiz.options.c + "</h2");
+    $("#optionsBox").append("<h2 class='choice' id='d'>" + objQuiz.options.d + "</h2>");
 
 }
 
+function correctDisplay() {
+    $("#questionTitle").text("Correct");
+    i++;
+    correctNum++;
+    currentQ = quizArray[i];
+    setTimeout(function(){ displayQuestion (currentQ); }, 3000);
+}
+
+function wrongDisplay() {
+    //grabs correct answer
+    let correctA = $("#"+currentQ.correct).text();
+    $("#questionTitle").text("Wrong! The correct answer was: " +correctA);
+    i++;
+    wrongNum++;
+    currentQ = quizArray[i];
+    setTimeout(function(){ displayQuestion (currentQ); }, 3000);
+}
 
 $(document).ready(function () {
 
-    
-    let currentQ = quizArray[0];
+    displayQuestion(quizArray[i]);
 
-    displayQuestion(currentQ);
+    $("#optionsBox").on("click", ".choice", function () {
 
-    $("#optionsBox").on("click", ".choice", function (){
-          
-        if ($(this).attr("id") === currentQ.correct){
-
-            console.log("correct");
-        }else {
-            console.log("wrong");
+        //compares option id to correct answer
+        if ($(this).attr("id") === currentQ.correct) {
+            correctDisplay();
+        } else {
+            wrongDisplay();
         }
-        
-        
+
+
 
 
 
     })
 
-    
 
-    
+
+
 
 
 
